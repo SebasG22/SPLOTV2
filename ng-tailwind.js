e76e3c88b8cfaa0a766b802b320687b58afd60b1
Sebastián Guevara <rlxsebas@gmail.com>
2018-08-11 09:51:50 -0500

    [ Styles ] - Adding tailwind

---
+++ ng-tailwind.js
@@ -0,0 +1,5 @@
+module.exports = {
+  configJS: '/home/sebasg/Documents/splotv2/tailwind.js',
+  sourceCSS: '/home/sebasg/Documents/splotv2/tailwind-build.css',
+  outputCSS: '/home/sebasg/Documents/splotv2/src/tailwind.css'
+}
