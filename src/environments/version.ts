c027033a964446e3d5029a823781d4a24ea1e943
Sebastián Guevara <rlxsebas@gmail.com>
2018-08-14 12:39:14 -0500

    [ Versions ] - Use for releases

---
+++ src/environments/version.ts
@@ -0,0 +1,14 @@
+// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+/* tslint:disable */
+export const VERSION = {
+    "dirty": true,
+    "raw": "291365240d4b-dirty",
+    "hash": "291365240d4b",
+    "distance": null,
+    "tag": null,
+    "semver": null,
+    "suffix": "291365240d4b-dirty",
+    "semverString": null,
+    "version": "0.0.0"
+};
+/* tslint:enable */
