03b875342b23fb3b119ddf8593dcffbc2ceb1539
Sebastian Velasquez Guevara <rlxsebas@gmail.com>
2018-10-29 14:14:06 -0500

    feature: projects - Adding actions to configuration module

--- src/app/configuration-process/actions/configuration-process.actions.ts
+++ src/app/configuration-process/actions/configuration-process.actions.ts
@@ -1,10 +1,17 @@
 import { Action } from '@ngrx/store';
-import { IConfigurationModel } from 'src/app/projects/models';
+import { IConfigurationModel, IConfigurationChildrenParsed, IUserDecision } from '../models/configuration-process.model';
 
 export const GET_CONFIGURATION_MODELS = '[ Configuration ] - Get configuration models';
 export const GET_CONFIGURATION_MODELS_SUCCESS = '[ Configuration ] - Get configuration models success';
 export const GET_CONFIGURATION_MODELS_FAILED = '[ Configuration ] - Get configuration models failed';
 
+export const GET_CHILDREN_CONFIGURATION_BY_LEVEL = '[ Configuration ] - Get children by level';
+export const GET_CHILDREN_CONFIGURATION_BY_LEVEL_SUCCESS = '[ Configuration ] - Get children by level success';
+export const GET_CHILDREN_CONFIGURATION_BY_LEVEL_FAILED = '[ Configuration ] - Get children by level failed';
+
+export const SAVE_USER_CONFIGURATION_DECISION = '[ Configuration ] - Save user configuration decision';
+export const SAVE_USER_CONFIGURATION_DECISION_SUCCESS = '[ Configuration ] - Save user configuration decision success';
+export const SAVE_USER_CONFIGURATION_DECISION_FAILED = '[ Configuration ] - Save user configuration decision failed';
 export class GetConfigurationModels implements Action {
     readonly type = GET_CONFIGURATION_MODELS;
     public constructor() { }
@@ -20,6 +27,42 @@ export class GetConfigurationModelsFailed implements Action {
     public constructor(public payload: string) { }
 }
 
+export class GetChildrenByLevel implements Action {
+    readonly type = GET_CHILDREN_CONFIGURATION_BY_LEVEL;
+    public constructor(public payload: number) {}
+}
+
+export class GetChildrenByLevelSuccess implements Action {
+    readonly type = GET_CHILDREN_CONFIGURATION_BY_LEVEL_SUCCESS;
+    public constructor(public payload: IConfigurationChildrenParsed) {}
+}
+
+export class GetChildrenByLevelFailed implements Action {
+    readonly type = GET_CHILDREN_CONFIGURATION_BY_LEVEL_FAILED;
+    public constructor(public payload: IConfigurationChildrenParsed) {}
+}
+
+export class SaveUserConfigurationDecision implements Action {
+    readonly type = SAVE_USER_CONFIGURATION_DECISION;
+    public constructor(public payload: IUserDecision) {}
+}
+
+export class SaveUserConfigurationDecisionSuccess implements Action {
+    readonly type = SAVE_USER_CONFIGURATION_DECISION_SUCCESS;
+    public constructor() {}
+}
+
+export class SaveUserConfigurationDecisionFailed implements Action {
+    readonly type = SAVE_USER_CONFIGURATION_DECISION_FAILED;
+    public constructor() {}
+}
+
 export type All = GetConfigurationModels |
     GetConfigurationModelsSuccess |
-    GetConfigurationModelsFailed;
+    GetConfigurationModelsFailed |
+    GetChildrenByLevel |
+    GetChildrenByLevelSuccess |
+    GetChildrenByLevelFailed |
+    SaveUserConfigurationDecision |
+    SaveUserConfigurationDecisionSuccess |
+    SaveUserConfigurationDecisionFailed;
